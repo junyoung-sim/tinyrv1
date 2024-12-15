3e34929f2d7095496d7a7a572e5ce9e29ff7ba67
junyoung-sim <jysim.ny@gmail.com>
2024-12-15 00:46:20 -0500

    Register_RTL

---
+++ hw/Register_RTL.v
@@ -0,0 +1,24 @@
+`ifndef REGISTER_RTL_V
+`define REGISTER_RTL_V
+
+module Register_RTL
+#(
+  parameter nbits=32
+)(
+  (* keep=1 *) input  logic             clk,
+  (* keep=1 *) input  logic             rst,
+  (* keep=1 *) input  logic             en,
+  (* keep=1 *) input  logic [nbits-1:0] d,
+  (* keep=1 *) output logic [nbits-1:0] q
+);
+
+  always_ff @(posedge clk) begin
+    if(rst)
+      q <= {nbits{1'b0}};
+    else
+      q <= (en ? d : q);
+  end
+
+endmodule
+
+`endif
