3226a04289f82900759ae9fb86ab57d288c64e72
junyoung-sim <jysim.ny@gmail.com>
2024-12-28 12:57:10 -0500

    improved ADD bypass testing

--- test/Proc_test.py
+++ test/Proc_test.py
@@ -114,10 +114,11 @@ async def test_add_raw_bypass(dut):
   # Assembly Program
 
   await asm_write(dut, 0x000, "add x1 x0 x0") # F D X M W
-  await asm_write(dut, 0x004, "add x2 x1 x0") #   F D X M W       (X-D)
-  await asm_write(dut, 0x008, "add x3 x1 x2") #     F D X M W     (M-D, X-D)
+  await asm_write(dut, 0x004, "add x2 x1 x0") #   F D X M W         (X-D)
+  await asm_write(dut, 0x008, "add x3 x1 x2") #     F D X M W       (M-D, X-D)
   await asm_write(dut, 0x00c, "add x0 x0 x0") #       F D X M W
-  await asm_write(dut, 0x010, "add x1 x2 x0") #         F D X M W (W-D)
+  await asm_write(dut, 0x010, "add x4 x2 x0") #         F D X M W   (W-D)
+  await asm_write(dut, 0x014, "add x5 x1 x1") #           F D X M W
   await reset(dut)
 
   # Check Traces
@@ -125,6 +126,7 @@ async def test_add_raw_bypass(dut):
   await check_trace(dut, x)
   await check_trace(dut, x)
   await check_trace(dut, x)
+  await check_trace(dut, 0)
   await check_trace(dut, 0)
   await check_trace(dut, 0)
   await check_trace(dut, 0)
