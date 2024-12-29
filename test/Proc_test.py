11e3dd8b94a85b0d33103c3012c7e6bf45d88e9a
junyoung-sim <jysim.ny@gmail.com>
2024-12-29 14:52:30 -0500

    ADDI

--- test/Proc_test.py
+++ test/Proc_test.py
@@ -18,6 +18,9 @@ FUNCT3 = 0b00000000000000000111000000000000 # 14:12
 RD     = 0b00000000000000000000111110000000 # 11:7
 OPCODE = 0b00000000000000000000000001111111 #  6:0
 
+def get_imm_i(imm):
+  return ((imm & 0b111111111111) << 20) & IMM_I
+
 def get_funct7(funct7):
   return (funct7 << 25) & FUNCT7
 
@@ -40,6 +43,7 @@ def get_opcode(opcode):
 # Instruction Assembly
 #===========================================================
 
+# add rd rs1 rs2
 def asm_add(inst_s):
   funct7 = get_funct7(0b0000000)
   rs2    = get_RS2(int(inst_s[3][1:]))
@@ -49,11 +53,22 @@ def asm_add(inst_s):
   opcode = get_opcode(0b0110011)
   return funct7 | rs2 | rs1 | funct3 | rd | opcode
 
+# addi rd rs1 imm
+def asm_addi(inst_s):
+  imm_i  = get_imm_i(int(inst_s[3]))
+  rs1    = get_RS1(int(inst_s[2][1:]))
+  funct3 = get_funct3(0b000)
+  rd     = get_RD(int(inst_s[1][1:]))
+  opcode = get_opcode(0b0010011)
+  return imm_i | rs1 | funct3 | rd | opcode
+
 def asm(inst_s):
   inst_s = inst_s.split()
 
   if inst_s[0] == "add":
     inst = asm_add(inst_s)
+  elif inst_s[0] == "addi":
+    inst = asm_addi(inst_s)
   
   return inst
 
@@ -97,6 +112,7 @@ async def test_add_simple(dut):
   # Assembly Program
 
   await asm_write(dut, 0x000, "add x1 x0 x0")
+
   await reset(dut)
 
   # Check Traces
@@ -119,6 +135,7 @@ async def test_add_raw_bypass(dut):
   await asm_write(dut, 0x00c, "add x0 x0 x0") #       F D X M W
   await asm_write(dut, 0x010, "add x4 x2 x0") #         F D X M W   (W-D)
   await asm_write(dut, 0x014, "add x5 x1 x1") #           F D X M W
+
   await reset(dut)
 
   # Check Traces
@@ -132,3 +149,108 @@ async def test_add_raw_bypass(dut):
   await check_trace(dut, 0)
   await check_trace(dut, 0)
   await check_trace(dut, 0)
+
+#===========================================================
+# ADDI
+#===========================================================
+
+@cocotb.test()
+async def test_addi_simple(dut):
+  clock = Clock(dut.clk, 10, units="ns")
+  cocotb.start_soon(clock.start(start_high=False))
+
+  # Assembly Program
+
+  await asm_write(dut, 0x000, "addi x1 x0 1") # F D X M W
+  await asm_write(dut, 0x004, "addi x2 x0 2") #   F D X M W
+
+  await reset(dut)
+
+  # Check Traces
+
+  await check_trace(dut, x)
+  await check_trace(dut, x)
+  await check_trace(dut, x)
+  await check_trace(dut, 1)
+  await check_trace(dut, 2)
+
+@cocotb.test()
+async def test_addi_imm_bound_valid(dut):
+  clock = Clock(dut.clk, 10, units="ns")
+  cocotb.start_soon(clock.start(start_high=False))
+
+  # Assembly Program
+
+  await asm_write(dut, 0x000, "addi x1 x0 2047" ) # F D X M W
+  await asm_write(dut, 0x004, "addi x1 x0 -2048") #   F D X M W
+
+  await reset(dut)
+
+  # Check Traces
+
+  await check_trace(dut, x)
+  await check_trace(dut, x)
+  await check_trace(dut, x)
+  await check_trace(dut,  2047 & 0xffffffff)
+  await check_trace(dut, -2048 & 0xffffffff)
+
+@cocotb.test()
+async def test_addi_imm_bound_invalid_1(dut):
+  clock = Clock(dut.clk, 10, units="ns")
+  cocotb.start_soon(clock.start(start_high=False))
+
+  # Assembly Program
+
+  await asm_write(dut, 0x000, "addi x1 x0 2048") # F D X M W
+
+  await reset(dut)
+
+  # Check Traces
+
+  await check_trace(dut, x)
+  await check_trace(dut, x)
+  await check_trace(dut, x)
+  await check_trace(dut, 2048) # FAIL
+
+@cocotb.test()
+async def test_addi_imm_bound_invalid_2(dut):
+  clock = Clock(dut.clk, 10, units="ns")
+  cocotb.start_soon(clock.start(start_high=False))
+
+  # Assembly Program
+
+  await asm_write(dut, 0x000, "addi x1 x0 -2049") # F D X M W
+
+  await reset(dut)
+
+  # Check Traces
+
+  await check_trace(dut, x)
+  await check_trace(dut, x)
+  await check_trace(dut, x)
+  await check_trace(dut, -2049) # FAIL
+
+@cocotb.test()
+async def test_addi_raw_bypass(dut):
+  clock = Clock(dut.clk, 10, units="ns")
+  cocotb.start_soon(clock.start(start_high=False))
+
+  # Assembly Program
+
+  await asm_write(dut, 0x000, "addi x1 x0 1") # F D X M W
+  await asm_write(dut, 0x004, "addi x2 x0 2") #   F D X M W
+  await asm_write(dut, 0x008, "add x3 x1 x2") #     F D X M W     (M-D, X-D)
+  await asm_write(dut, 0x00c, "addi x4 x1 3") #       F D X M W   (W-D)
+  await asm_write(dut, 0x010, "addi x5 x1 4") #         F D X M W
+
+  await reset(dut)
+
+  # Check Traces
+  await check_trace(dut, x)
+  await check_trace(dut, x)
+  await check_trace(dut, x)
+  await check_trace(dut, 1)
+  await check_trace(dut, 2)
+  await check_trace(dut, 3)
+  await check_trace(dut, 4)
+  await check_trace(dut, 5)
