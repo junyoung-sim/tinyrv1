7bc61ff3e95573481c3f78b0b99a9aaeb4a1fdd5
junyoung-sim <jysim.ny@gmail.com>
2025-01-01 16:04:27 -0500

    simplified LW test

--- test/Proc_lw_test.py
+++ test/Proc_lw_test.py
@@ -7,30 +7,6 @@ async def test_simple(dut):
 
   # Data Memory
 
-  await data(dut, 0x080, 0xcafe2300)
-
-  # Assembly Program
-
-  await asm_write(dut, 0x000, "addi x1 x0 0x080") # F D X M W
-  await asm_write(dut, 0x004, "lw x2 0(x1)"     ) #   F D X M W (X-D)
-
-  await reset(dut)
-
-  # Check Traces
-
-  await check_trace(dut, x)
-  await check_trace(dut, x)
-  await check_trace(dut, x)
-  await check_trace(dut, 0x00000080)
-  await check_trace(dut, 0xcafe2300)
-
-@cocotb.test()
-async def test_imm(dut):
-  clock = Clock(dut.clk, 10, units="ns")
-  cocotb.start_soon(clock.start(start_high=False))
-
-  # Data Memory
-
   await data(dut, 0x07c, 0xcafe2300)
   await data(dut, 0x080, 0xacedbeef)
   await data(dut, 0x084, 0xdeafface)
