b06ec9cc598deeeddbda805bf4e204e28f9413e4
junyoung-sim <jysim.ny@gmail.com>
2025-01-03 11:18:22 -0500

    cs comment

--- hw/ProcCtrl.v
+++ hw/ProcCtrl.v
@@ -338,10 +338,10 @@ module ProcCtrl
     if(val_M) begin
       casez(inst_M)
         //           dval  dtype wb
-        `ADD  : cs_M( 0,   'x,   0 ); // result_X       (RF)
-        `ADDI : cs_M( 0,   'x,   0 ); // result_X       (RF)
-        `MUL  : cs_M( 0,   'x,   0 ); // result_X       (RF)
-        `LW   : cs_M( 1,    0,   1 ); // dmemresp_rdata (RF)
+        `ADD  : cs_M( 0,   'x,   0 ); // result_X
+        `ADDI : cs_M( 0,   'x,   0 ); // result_X
+        `MUL  : cs_M( 0,   'x,   0 ); // result_X
+        `LW   : cs_M( 1,    0,   1 ); // dmemresp_rdata (M)
         `SW   : cs_M( 1,    1,  'x ); // dmemreq_wdata  (M)
 
         default: cs_M( 'x, 'x, 'x );
