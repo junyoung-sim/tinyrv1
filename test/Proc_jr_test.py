0d2beaa63dd8f0384f5b79c24a69d99359cf92d1
junyoung-sim <jysim.ny@gmail.com>
2025-01-03 20:28:43 -0500

    JR tests complete

--- test/Proc_jr_test.py
+++ test/Proc_jr_test.py
@@ -1,7 +1,7 @@
 from TinyRV1 import *
 
 @cocotb.test()
-async def test_simple(dut):
+async def test_count(dut):
   clock = Clock(dut.clk, 10, units="ns")
   cocotb.start_soon(clock.start(start_high=False))
 
@@ -12,12 +12,12 @@ async def test_simple(dut):
   await asm_write(dut, 0x008, "addi x2 x2 0x001") #     F D X M W               (X-D)
   await asm_write(dut, 0x00c, "jr x1"           ) #       F D X M W             (W-D)
   await asm_write(dut, 0x010, "add x0 x0 x0"    ) #         F - - - -
-  #                            addi x2 x2 1                   F D X M W         (W-D)
-  #                            jr x1                            F D X M W
-  #                            add x0 x0 x0                       F - - - -
-  #                            addi x2 x2 1                         F D X M W   (W-D)
-  #                            jr x1                                  F D X M W
-  #                            ...
+  #                            addi x2 x2 0x001   #           F D X M W         (W-D)
+  #                            jr x1              #             F D X M W
+  #                            add x0 x0 x0       #               F - - - -
+  #                            addi x2 x2 0x001   #                 F D X M W   (W-D)
+  #                            jr x1              #                   F D X M W
+  #                                 ...           #                      ...
 
   await reset(dut)
 
@@ -26,17 +26,72 @@ async def test_simple(dut):
   await check_trace(dut, x)
   await check_trace(dut, x)
   await check_trace(dut, x)
-  await check_trace(dut, 0x008) # addi x1 x0 0x008
-  await check_trace(dut, 0x000) # addi x2 x0 0x000
-  await check_trace(dut, 0x001) # addi x2 x2 0x001
-  await check_trace(dut, 0x001) # jr x1
-  await check_trace(dut, 0x001) # add x0 x0 x0
-  await check_trace(dut, 0x002) # addi x2 x2 1
-  await check_trace(dut, 0x002) # jr x1
-  await check_trace(dut, 0x002) # addi x0 x0 x0
-  await check_trace(dut, 0x003) # addi x2 x2 1
-  await check_trace(dut, 0x003) # jr x1
-  await check_trace(dut, 0x003) # addi x0 x0 x0
-  await check_trace(dut, 0x004) # addi x2 x2 1
-  await check_trace(dut, 0x004) # jr x1
-  await check_trace(dut, 0x004) # addi x0 x0 x0
+  await check_trace(dut, 0x008)
+  await check_trace(dut, 0x000)
+  await check_trace(dut, 0x001)
+  await check_trace(dut, 0x001)
+  await check_trace(dut, 0x001)
+  await check_trace(dut, 0x002)
+  await check_trace(dut, 0x002)
+  await check_trace(dut, 0x002)
+  await check_trace(dut, 0x003)
+  await check_trace(dut, 0x003)
+  await check_trace(dut, 0x003)
+  await check_trace(dut, 0x004)
+  await check_trace(dut, 0x004)
+  await check_trace(dut, 0x004)
+
+@cocotb.test()
+async def test_loop(dut):
+  clock = Clock(dut.clk, 10, units="ns")
+  cocotb.start_soon(clock.start(start_high=False))
+
+  # Assembly
+
+  await asm_write(dut, 0x000, "addi x1 x0 0x014") # F D X M W
+  await asm_write(dut, 0x004, "jr x1"           ) #   F D X M W                    (X-D)
+  await asm_write(dut, 0x008, "add x0 x0 x0"    ) #     F - - - -
+  await asm_write(dut, 0x00c, "add x0 x0 x0"    ) #
+  await asm_write(dut, 0x010, "add x0 x0 x0"    ) #
+  await asm_write(dut, 0x014, "addi x1 x1 -20"  ) #       F D X M W                (W-D)
+  await asm_write(dut, 0x018, "jr x1"           ) #         F D X M W              (X-D)
+  await asm_write(dut, 0x01c, "add x0 x0 x0"    ) #           F - - - -
+  await asm_write(dut, 0x01c, "add x0 x0 x0"    ) #
+  await asm_write(dut, 0x01c, "add x0 x0 x0"    ) #
+  #                            addi x1 x0 0x014   #             F D X M W
+  #                            jr x1              #               F D X M W        (X-D)
+  #                            add x0 x0 x0       #                 F - - - -
+  #                            add x0 x0 x0       #
+  #                            add x0 x0 x0       #
+  #                            addi x1 x1 -20     #                   F D X M W    (W-D)
+  #                            jr x1              #                     F D X M W  (X-D)
+  #                            add x0 x0 x0       #                       F - - - -
+  #                            add x0 x0 x0       #
+  #                            add x0 x0 x0       #
+  #                                 ...           #                          ...
+
+  await reset(dut)
+
+  # Check Traces
+
+  await check_trace(dut, x)
+  await check_trace(dut, x)
+  await check_trace(dut, x)
+  await check_trace(dut, 0x014)
+  await check_trace(dut, 0x014)
+  await check_trace(dut, 0x014)
+  await check_trace(dut, 0x000)
+  await check_trace(dut, 0x000)
+  await check_trace(dut, 0x000)
+  await check_trace(dut, 0x014)
+  await check_trace(dut, 0x014)
+  await check_trace(dut, 0x014)
+  await check_trace(dut, 0x000)
+  await check_trace(dut, 0x000)
+  await check_trace(dut, 0x000)
+  await check_trace(dut, 0x014)
+  await check_trace(dut, 0x014)
+  await check_trace(dut, 0x014)
+  await check_trace(dut, 0x000)
+  await check_trace(dut, 0x000)
+  await check_trace(dut, 0x000)
