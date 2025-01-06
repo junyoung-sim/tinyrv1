5cf28ab3660cac86ab702c3cd45ae9111e6669b6
junyoung-sim <jysim.ny@gmail.com>
2025-01-06 17:47:05 -0500

    improved JAL test

--- test/Proc_jal_test.py
+++ test/Proc_jal_test.py
@@ -32,22 +32,21 @@ async def test_simple(dut):
   await check_trace(dut, 0x004)
 
 @cocotb.test()
-async def test_count(dut):
+async def test_forward_jal(dut):
   clock = Clock(dut.clk, 10, units="ns")
   cocotb.start_soon(clock.start(start_high=False))
 
   # Assembly Program
 
-  await asm_write(dut, 0x000, "addi x2 x0 0") # F D X M W
-  await asm_write(dut, 0x004, "jal x1 0x00c") #   F D X M W
-  await asm_write(dut, 0x008, "addi x2 x2 1") #     F - - - -
-  await asm_write(dut, 0x00c, "jr x1"       ) #       F D X M W           (M-D)
-  await asm_write(dut, 0x010, "add x0 x0 x0") #         F - - - -
-  #                            addi x2 x2 1   #           F D X M W
+  await asm_write(dut, 0x000, "jal x1 0x00c") # F D X M W
+  await asm_write(dut, 0x004, "add x0 x0 x0") #   F - - - -
+  await asm_write(dut, 0x008, "addi x2 x0 1")
+  await asm_write(dut, 0x00c, "jr x1"       ) #     F D X M W         (M-D)
+  await asm_write(dut, 0x010, "add x0 x0 x0") #       F - - - -
+  #                            add x0 x0 x0   #         F D X M W
+  #                            addi x2 x0 1   #           F D X M W
   #                            jr x1          #             F D X M W
-  #                            add x0 x0 x0   #               F - - - -
-  #                            addi x2 x2 1   #                 F D X M W (W-D)
-  #                                 ...       #                    ...
+  #                                 ...       #                ...
 
   await reset(dut)
 
@@ -56,17 +55,52 @@ async def test_count(dut):
   await check_trace(dut, x)
   await check_trace(dut, x)
   await check_trace(dut, x)
+  await check_trace(dut, 0x004)
+  await check_trace(dut, 0x004)
+  await check_trace(dut, 0x004)
+  await check_trace(dut, 0x004)
   await check_trace(dut, 0x000)
-  await check_trace(dut, 0x008)
-  await check_trace(dut, 0x008)
-  await check_trace(dut, 0x008)
-  await check_trace(dut, 0x008)
   await check_trace(dut, 0x001)
   await check_trace(dut, 0x001)
   await check_trace(dut, 0x001)
-  await check_trace(dut, 0x002)
-  await check_trace(dut, 0x002)
-  await check_trace(dut, 0x002)
-  await check_trace(dut, 0x003)
-  await check_trace(dut, 0x003)
-  await check_trace(dut, 0x003)
+  await check_trace(dut, 0x000)
+  await check_trace(dut, 0x001)
+  await check_trace(dut, 0x001)
+  await check_trace(dut, 0x001)
+
+@cocotb.test()
+async def test_backward_jal(dut):
+  clock = Clock(dut.clk, 10, units="ns")
+  cocotb.start_soon(clock.start(start_high=False))
+
+  # Assembly Program
+
+  await asm_write(dut, 0x000, "jal x1 0x010") # F D X M W
+  await asm_write(dut, 0x004, "addi x2 x0 1") #   F - - - -
+  await asm_write(dut, 0x008, "addi x1 x1 4")
+  await asm_write(dut, 0x00c, "jr x1"       )
+  await asm_write(dut, 0x010, "jal x1 0x004") #     F D X M W
+  await asm_write(dut, 0x014, "add x0 x0 x0") #       F - - - -
+  await asm_write(dut, 0x018, "addi x2 x0 1")
+  #                            addi x2 x0 1   #         F D X M W
+  #                            addi x1 x1 4   #           F D X M W      (W-D)
+  #                            jr x1          #             F D X M W    (X-D)
+  #                            jal x1 0x004   #               F - - - -
+  #                            addi x2 x0 1   #                 F D X M W
+
+  await reset(dut)
+
+  # Check Traces
+
+  await check_trace(dut, x)
+  await check_trace(dut, x)
+  await check_trace(dut, x)
+  await check_trace(dut, 0x004)
+  await check_trace(dut, 0x004)
+  await check_trace(dut, 0x014)
+  await check_trace(dut, 0x014)
+  await check_trace(dut, 0x001)
+  await check_trace(dut, 0x018)
+  await check_trace(dut, 0x018)
+  await check_trace(dut, 0x018)
+  await check_trace(dut, 0x001)
