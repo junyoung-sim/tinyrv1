cfb7c0334d36f66e78cdda8351a2018b807b74d9
junyoung-sim <jysim.ny@gmail.com>
2025-01-18 12:12:22 -0500

    successfully synthesized processor in quartus!

---
+++ fpga/BinaryToBinCodedDec_GL.v
@@ -0,0 +1,41 @@
+`ifndef BINARY_TO_BIN_CODED_DEC_GL_V
+`define BINARY_TO_BIN_CODED_DEC_GL_V
+
+module BinaryToBinCodedDec_GL
+(
+  (* keep=1 *) input  wire [4:0] in,
+  (* keep=1 *) output wire [3:0] tens,
+  (* keep=1 *) output wire [3:0] ones
+);
+
+  wire d4, d3, d2, d1, d0;
+  assign d4 = in[4];
+  assign d3 = in[3];
+  assign d2 = in[2];
+  assign d1 = in[1];
+  assign d0 = in[0];
+
+  assign tens[3] = 0;
+  assign tens[2] = 0;
+  assign tens[1] =  d4 & (d3 | d2);
+  assign tens[0] = ~d4 &  d3 & (d2 | d1) | 
+                    d4 & ~d3 & ~d2       |
+                    d4 &  d3 &  d2 & d1  ;
+  
+  assign ones[3] = ~d4 &  d3 & ~d2 & ~d1 |
+                    d4 & ~d3 & ~d2 &  d1 |
+                    d4 &  d3 &  d2 & ~d1 ;
+  assign ones[2] = ~d4 & ~d3 &  d2       |
+                   ~d4 &  d3 &  d2 &  d1 |
+                    d4 & ~d3 & ~d2 & ~d1 |
+                    d4 &  d3 & ~d2       ;
+  assign ones[1] = ~d4 & ~d3 &  d1       |
+                   ~d4 &  d3 &  d2 & ~d1 |
+                    d4 & ~d3 & ~d2 & ~d1 |
+                    d4 & ~d3 &  d2 &  d1 |
+                    d4 &  d3 & ~d2 &  d1 ;
+  assign ones[0] = d0;
+
+endmodule
+
+`endif
---
+++ fpga/BinaryToSevenSeg_GL.v
@@ -0,0 +1,31 @@
+`ifndef BINARY_TO_SEVEN_SEG_GL_V
+`define BINARY_TO_SEVEN_SEG_GL_V
+
+module BinaryToSevenSeg_GL
+(
+  (* keep=1 *) input  wire [3:0] in,
+  (* keep=1 *) output wire [6:0] seg
+);
+
+  wire d3, d2, d1, d0;
+  assign d3 = in[3];
+  assign d2 = in[2];
+  assign d1 = in[1];
+  assign d0 = in[0];
+
+  assign seg[6] = ~(d3 | d2 | d1) | ~d3 & d2 & d1 & d0;
+  assign seg[5] = ~d3 &  d1 & d0  | ~(d3 | d2) & (d1 | d0);
+  assign seg[4] = ~d2 & ~d1 & d0  | ~d3 & (d2 & ~d1 | d0);
+
+  assign seg[3] = ~d2 & ~d1 &  d0       |
+                  ~d3 &  d2 &  d1 &  d0 |
+                  ~d3 &  d2 & ~d1 & ~d0 ;
+  assign seg[2] = ~d3 & ~d2 &  d1 & ~d0 ;
+  assign seg[1] = ~d3 &  d2 & ~d1 &  d0 |
+                  ~d3 &  d2 &  d1 & ~d0 ;
+  assign seg[0] = ~d3 &  d2 & ~d1 & ~d0 |
+                  ~d3 & ~d2 & ~d1 &  d0 ;
+
+endmodule
+
+`endif
