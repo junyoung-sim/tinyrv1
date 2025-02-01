8158fe701b3fbde7c978e14f4f90a9b35d1ece72
junyoung-sim <jysim.ny@gmail.com>
2025-01-31 21:06:24 -0500

    working on TestUtils...

---
+++ test/Proc_add_test.v
@@ -0,0 +1,23 @@
+`include "../hw/Proc.v"
+`include "TestMemory.v"
+`include "TestUtils.v"
+
+module Top();
+
+  // verilator lint_off UNUSED
+  logic clk;
+  logic rst;
+  // verilator lint_on UNUSED
+
+  TestUtils t
+  (
+    .*
+  );
+
+  //==========================================================
+  // DUT
+  //==========================================================
+
+  
+
+endmodule
